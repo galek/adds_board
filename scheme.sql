d2f50beabcce29591aff0e2c38cccb4aa95c9b4b
Nick Galko <nikolay.galko@gmail.com>
2016-11-20 21:00:11 +0300

    Added categoryID in postings

--- scheme.sql
+++ scheme.sql
@@ -1,3 +1,3 @@
 CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY, name TEXT);
-CREATE TABLE IF NOT EXISTS postings (id INTEGER PRIMARY KEY, cookie TEXT, caption TEXT, content TEXT, phonenumber TEXT, created INTEGER);
+CREATE TABLE IF NOT EXISTS postings (id INTEGER PRIMARY KEY, categoryId INTEGER, cookie TEXT, caption TEXT, content TEXT, phonenumber TEXT, created INTEGER);
 CREATE TABLE IF NOT EXISTS favorites (id INTEGER PRIMARY KEY, cookie TEXT, postingid INTEGER);
